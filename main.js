cbf02b80ce1e9e9365ebd8e80442c16a80fbbf5d
codingcele <marcocelegato9@gmail.com>
2022-11-07 17:58:32 +0100

    finito

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-alert(ciao);
+
+let km = prompt("How many chilometers?");
+
+let age = prompt("How old are you?");
+
+let prcPerKm = 0.21;
+
+let underageDsc = 0.20;
+
+let over65Dsc = 0.40;
+
+if (age < 18) {
+    price = km * prcPerKm * (1 - underageDsc)
+}
+else if (age > 65) {
+    price = km * prcPerKm * (1- over65Dsc)
+}
+else {
+    price = km * prcPerKm
+}
+
+let rndPrice = price.toFixed(2);
+
+
+document.getElementById("getPrice").innerHTML = `Your ticket's price is: ${rndPrice}`;
